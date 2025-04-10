7de40433c1aa38f879a3599f2af6d13bec7f6470
joshuaax22 <joshuaax22@gmail.com>
2025-04-10 23:17:00 +0000

    .env

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig, loadEnv } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-export default defineConfig({
-  plugins: [svelte()],
-  css: {
-    postcss: './postcss.config.js'
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  }
-}) 
+export default defineConfig(({ mode }) => {
+  loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [svelte()],
+    css: {
+      postcss: "./postcss.config.js",
+    },
+    build: {
+      outDir: "dist",
+      assetsDir: "assets",
+      emptyOutDir: true,
+    },
+  };
+});
