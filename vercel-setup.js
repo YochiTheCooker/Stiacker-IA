a4059982a357161d4b595b0581bb40eb9025e3d9
joshuaax22 <joshuaax22@gmail.com>
2025-04-10 22:48:30 +0000

    vamos a ver si si

--- vercel-setup.js
+++ vercel-setup.js
@@ -1,11 +1,7 @@
 import fs from 'fs';
 import { execSync } from 'child_process';
 
-console.log('🔄 Running Vercel setup...');
-
-// Comprobar si estamos en Vercel
-const isVercel = process.env.VERCEL === '1';
-console.log(`🔍 Running in Vercel environment: ${isVercel}`);
+console.log('🔄 Running Vercel setup...');;
 
 try {
   // Verificar que el directorio dist existe
@@ -18,33 +14,30 @@ try {
   
   // Listar archivos en dist para diagnóstico
   const filesDist = fs.readdirSync('./dist');
-  console.log('📑 Files in dist directory:', filesDist);
+  console.log('📑 Files in dist directory:', filesDist);  
+  // Crear estructura de directorios para Vercel
+  const vercelOutputDir = './.vercel/output/static';
+  const vercelConfigPath = './.vercel/output/config.json'
+  console.log(`📁 Creating ${vercelOutputDir} directory...`);
+  fs.mkdirSync(vercelOutputDir, { recursive: true });
 
-  if (isVercel) {
-    // Crear estructura de directorios para Vercel
-    const vercelOutputDir = './.vercel/output/static';
-    const vercelConfigPath = './.vercel/output/config.json'
-    console.log(`📁 Creating ${vercelOutputDir} directory...`);
-    fs.mkdirSync(vercelOutputDir, { recursive: true });
+  //Copiar todo el contenido de dist a .vercel/output/static
+  console.log('📋 Copying files to Vercel output directory...');
+  execSync(`cp -r dist/* ${vercelOutputDir}`);
 
-    //Copiar todo el contenido de dist a .vercel/output/static
-    console.log('📋 Copying files to Vercel output directory...');
-    execSync(`cp -r dist/* ${vercelOutputDir}`);
-
-    // Crear archivo config.json para Vercel
-    console.log('📝 Creating Vercel config.json...');
-    const vercelConfig = {
-      version: 3,
-      routes: [{ handle: 'filesystem' }, { src: '/.*', dest: '/index.html', status: 200 }],
-      output: {
-        path: '.vercel/output/static'
-      }
+  // Crear archivo config.json para Vercel
+  console.log('📝 Creating Vercel config.json...');
+  const vercelConfig = {
+    version: 3,
+    routes: [{ handle: 'filesystem' }, { src: '/.*', dest: '/index.html', status: 200 }],
+    output: {
+      path: '.vercel/output/static'
     }
-    fs.writeFileSync(vercelConfigPath, JSON.stringify(vercelConfig, null, 2));
-    // Crear archivo _redirects para SPA routing
-    console.log('📝 Creating SPA routing file...');
-    fs.writeFileSync(`${vercelOutputDir}/_redirects`, '/* /index.html 200');
   }
+  fs.writeFileSync(vercelConfigPath, JSON.stringify(vercelConfig, null, 2));
+  // Crear archivo _redirects para SPA routing
+  console.log('📝 Creating SPA routing file...');
+  fs.writeFileSync(`${vercelOutputDir}/_redirects`, '/* /index.html 200');
   
   console.log('✅ Setup completed successfully!');
 } catch (error) {
