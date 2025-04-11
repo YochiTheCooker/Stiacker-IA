fdcbc44d74d9e71adaf2e5752b3287eb6d6747d1
joshuaax22 <joshuaax22@gmail.com>
2025-04-11 17:29:57 +0000

    adios vercel host

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto';
 
 export default {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      runtime: 'nodejs18.x'
-    })
+    adapter: adapter()
   }
 };
